a36f0c6b4f1a512d5cd6b510bcbe96471ecf511a
David Andrés Herrera <biggiegun@gmail.com>
2021-09-22 21:50:33 -0500

    Cambios ejericio 20 septiembre

--- src/20Septiembre/Components/Counter.js
+++ src/20Septiembre/Components/Counter.js
@@ -12,13 +12,61 @@ const Counter = () => {
         increment:true
     }
 
-    const reducer = (state, action)=>{}
+    const reducer = (state, action)=>{
+        switch (action.type) {
+            case "INCREMENT":
+                return{
+                    ...state,
+                    increment: action.increment
+                }
+
+            case "SET_INTERVAL":
+            return{
+                ...state,
+                countInterval: parseInt(action.countInterval)
+            }
+
+            case "INCREASE_COUNT":
+            return{
+                ...state,
+                count: state.count + state.countInterval
+            }
+            
+            case "DECREASE_COUNT":
+            return{
+                ...state,
+                count: state.count - state.countInterval
+            }
+
+            case "RESTART":
+            return initialState
+
+            default:
+                return state;
+        }
+    }
 
     const [state, dispatch] = useReducer(reducer, initialState)
-    const handleIncrement = (e) =>{
-
-    }
     
+    const handleIncrement = (e) =>{
+        const {checked} = e.target;
+        dispatch({type:"INCREMENT", increment: checked})
+    }
+    const handleCountInterval = (e) =>{
+        const {value} = e.target;
+        dispatch({type:"SET_INTERVAL", countInterval: value})
+    }
+    const handlecount = (e) =>{
+        if(state.increment){
+        dispatch({type:"INCREASE_COUNT"})
+        }else{
+        dispatch({type:"DECREASE_COUNT"})
+        }
+    }
+    const handleRestart = (e) =>{
+        dispatch({type:"RESTART"})
+    }
+
     return (
         <div>
             <h1> Hook UseReducer</h1>
@@ -26,7 +74,7 @@ const Counter = () => {
             <div>
                 <input type="checkbox"
                 id="chk"
-                defaultChecked={state.increment}
+                checked={state.increment}
                 onChange={handleIncrement}
                 />
                 <label htmlFor="chk">
@@ -38,7 +86,15 @@ const Counter = () => {
             <label htmlFor="chk">
                 {"Intervalo"}
                 </label>
+                <input
+                type="text"
+                id="interval"
+                value={state.countInterval}
+                onChange={handleCountInterval}/>
             </div>
+            <br/>
+            <button onClick={handlecount}>{state.increment?"Incrementar":"Decrementar"}</button>
+            <button onClick={handleRestart}>Reiniciar</button>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import { MainApp } from './components/MainApp';
+//import { MainApp } from './components/MainApp';
+import Counter from './20Septiembre/Components/Counter'
 
 ReactDOM.render(
   <React.StrictMode>
-    <MainApp />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
